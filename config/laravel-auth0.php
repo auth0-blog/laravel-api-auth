7b1e78bf26e8b10a161e1b20a2bb3b456449db00
Holly Lloyd <holly.lloyd@auth0.com>
2021-05-05 15:00:59 -0700

    simplify for api only use

--- config/laravel-auth0.php
+++ config/laravel-auth0.php
@@ -1,79 +1,100 @@
 <?php
 
-return array(
+return [
 
     /*
-    |--------------------------------------------------------------------------
-    |   Your auth0 domain
-    |--------------------------------------------------------------------------
-    |   As set in the auth0 administration page
-    |
+        |--------------------------------------------------------------------------
+        |   Your auth0 domain
+        |--------------------------------------------------------------------------
+        |   As set in the auth0 administration page
+        |
     */
-
-    'domain'        => getenv('AUTH0_DOMAIN'),
-    /*
-    |--------------------------------------------------------------------------
-    |   Your APP id
-    |--------------------------------------------------------------------------
-    |   As set in the auth0 administration page
-    |
-    */
-
-    'client_id'     => getenv('AUTH0_CLIENT_ID'),
+    'domain'        => env( 'AUTH0_DOMAIN' ),
 
     /*
-    |--------------------------------------------------------------------------
-    |   Your APP secret
-    |--------------------------------------------------------------------------
-    |   As set in the auth0 administration page
-    |
+        |--------------------------------------------------------------------------
+        |   Your APP id
+        |--------------------------------------------------------------------------
+        |   As set in the auth0 administration page
+        |
     */
-    'client_secret' => getenv('AUTH0_CLIENT_SECRET'),
-
-
-   /*
-    |--------------------------------------------------------------------------
-    |   The redirect URI
-    |--------------------------------------------------------------------------
-    |   Should be the same that the one configure in the route to handle the
-    |   'Auth0\Login\Auth0Controller@callback'
-    |
-    */
-
-    'redirect_uri'  => getenv('AUTH0_CALLBACK_URL'),
+    // 'client_id'     => env( 'AUTH0_CLIENT_ID' ),
 
     /*
-    |--------------------------------------------------------------------------
-    |   Persistence Configuration
-    |--------------------------------------------------------------------------
-    |   persist_user            (Boolean) Optional. Indicates if you want to persist the user info, default true
-    |   persist_access_token    (Boolean) Optional. Indicates if you want to persist the access token, default false
-    |   persist_id_token        (Boolean) Optional. Indicates if you want to persist the id token, default false
-    |
+        |--------------------------------------------------------------------------
+        |   Your APP secret
+        |--------------------------------------------------------------------------
+        |   As set in the auth0 administration page
+        |
     */
-
-    // 'persist_user' => true,
-    // 'persist_access_token' => false,
-    // 'persist_id_token' => false,
+    // 'client_secret' => env( 'AUTH0_CLIENT_SECRET' ),
 
     /*
-    |--------------------------------------------------------------------------
-    |   The authorized token issuers
-    |--------------------------------------------------------------------------
-    |   This is used to verify the decoded tokens when using RS256
-    |
+        |--------------------------------------------------------------------------
+        |   The redirect URI
+        |--------------------------------------------------------------------------
+        |   Should be the same that the one configure in the route to handle the
+        |   'Auth0\Login\Auth0Controller@callback'
+        |
     */
-    'authorized_issuers'  => [ 'https://'.getenv('AUTH0_DOMAIN').'/' ],
+    'redirect_uri'  => env( 'APP_URL' ).'/auth0/callback',
 
     /*
-    |--------------------------------------------------------------------------
-    |   The api identifier
-    |--------------------------------------------------------------------------
-    |   This is used to verify the decoded tokens when using RS256
-    |
+        |--------------------------------------------------------------------------
+        |   Persistence Configuration
+        |--------------------------------------------------------------------------
+        |   persist_user            (Boolean) Optional. Indicates if you want to persist the user info, default true
+        |   persist_access_token    (Boolean) Optional. Indicates if you want to persist the access token, default false
+        |   persist_refresh_token   (Boolean) Optional. Indicates if you want to persist the refresh token, default false
+        |   persist_id_token        (Boolean) Optional. Indicates if you want to persist the id token, default false
+        |
     */
-    'api_identifier'  => getenv('API_IDENTIFIER'),
+    'persist_user' => true,
+    'persist_access_token' => false,
+    'persist_refresh_token' => false,
+    'persist_id_token' => false,
 
-    'supported_algs' => ['RS256']
+    /*
+        |--------------------------------------------------------------------------
+        |   The authorized token audiences
+        |--------------------------------------------------------------------------
+        |
+    */
+    'api_identifier'  => env( 'API_IDENTIFIER' ),
 
-);
+
+    /*
+        |--------------------------------------------------------------------------
+        |   Auth0 Organizations
+        |--------------------------------------------------------------------------
+        |   organization (string) Optional. Id of an Organization, if being used. Used when generating log in urls and validating token claims.
+    */
+    // 'organization'  => '',
+
+    /*
+        |--------------------------------------------------------------------------
+        |   The secret format
+        |--------------------------------------------------------------------------
+        |   Used to know if it should decode the secret when using HS256
+        |
+    */
+    'secret_base64_encoded'  => false,
+
+    /*
+        |--------------------------------------------------------------------------
+        |   Supported algorithms
+        |--------------------------------------------------------------------------
+        |   Token decoding algorithms supported by your API
+        |
+    */
+    'supported_algs'        => [ 'RS256' ],
+
+    /*
+        |--------------------------------------------------------------------------
+        |   Guzzle Options
+        |--------------------------------------------------------------------------
+        |   guzzle_options    (array) optional. Used to specify additional connection options e.g. proxy settings
+        |
+    */
+    // 'guzzle_options' => []
+];
